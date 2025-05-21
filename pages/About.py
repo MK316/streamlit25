0a0725520cfbda96866ec8c71b730188c64104f0
MK <99416359+MK316@users.noreply.github.com>
2025-05-21 13:40:58 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -5,3 +5,27 @@ st.title("This app is under construction.")
 url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
 st.image(url, caption="Image link", width=300)  # width in pixels
 
+st.markdown("""
+
+# Lesson Planning
++ main page : https://github.com/KY7437/G01Final
++ plan lesson in a week(til monday)
++ the revision after the presentation 
++ Overview: middle school student
++ Urls: https://s25g01.streamlit.app/
++ objectives:
++ tools(streamlit main):TTS, Wordle, Word Frequency, AI image generator...
+# 2025. 05. 19 Updated
+## In the end of this lesson, ...
++ Students will be able to understand the passage.
++ Students will be able to understand and apply the grammar rules.
++ Students will be able to memorize nessasary words.
++ Students will be able to ...
+# Vocabulary
+## Word Frequency; fork from MK316
++ App Discriptions: Make a cvs file from the whole passage, elimitating undesirable words and phrases.
++ Advantages : make it easy to have a word list, and the teachers can select which words they want to teach.
++ Concerns : Students may use the app, but students may not distinguish which words is important.
+
+""")
+
